ec25ffcf83d7fb440405d56465236c14712953f5
mwang20 <mwang20@stuy.edu>
2020-12-14 00:51:56 -0500

    I overrode the add() method.

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,6 +1,14 @@
 import java.lang.IllegalArgumentException;
 import java.util.ArrayList;
 
-public class NoNullArrayList{
-  
+public class NoNullArrayList<T> extends ArrayList<T>{
+
+  public boolean add(T value){
+    if (value == null){
+      throw new IllegalArgumentException();
+    }
+    super.add(value);
+    return true;
+  }
+
 }
