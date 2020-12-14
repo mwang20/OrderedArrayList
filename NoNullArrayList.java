b11c4b610fd0d94e38a6865f3cc49c3e68b16b8e
mwang20 <mwang20@stuy.edu>
2020-12-14 09:05:20 -0500

    Created the startingCapacity constructor.

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -2,11 +2,14 @@ import java.lang.IllegalArgumentException;
 import java.util.ArrayList;
 
 public class NoNullArrayList<T> extends ArrayList<T>{
-  
+
   public NoNullArrayList(){
     super();
   }
 
+  public NoNullArrayList(int startingCapacity){
+    super(startingCapacity);
+  }
   public boolean add(T value){
     if (value == null){
       throw new IllegalArgumentException();
