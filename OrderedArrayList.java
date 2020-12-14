2dca14fb0775706e81d03f16a447755fde869611
mwang20 <mwang20@stuy.edu>
2020-12-14 09:33:51 -0500

    Created an instance variable that would compare the value to the other values of teh array and place it accordingly.

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,3 +1,18 @@
+import java.lang.IllegalArgumentException;
+import java.util.ArrayList;
+
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
-  
+
+  private int ordered(T value){
+    if (value == null){
+      throw new IllegalArgumentException();
+    }
+    for (int i = 0; i < super.size(); i++){
+      if (value.compareTo(super.get(i)) < 0){
+        return i;
+      }
+    }
+    return super.size();
+  }
+
 }
